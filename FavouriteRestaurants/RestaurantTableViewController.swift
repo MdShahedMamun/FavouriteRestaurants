255cb2ab6a7723324ce505969a3fbfb07a625115
MdShahedMamun <shahed.cse12@gmail.com>
2016-11-24 03:40:57 +0600

    Cell animation added

--- FavouriteRestaurants/RestaurantTableViewController.swift
+++ FavouriteRestaurants/RestaurantTableViewController.swift
@@ -11,9 +11,11 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     var searchController:UISearchController!
     var searchResults:[Restaurant] = []
     
+    var postShown = [Bool](count: 10000, repeatedValue: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Remove the title of the back button
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
         
@@ -49,27 +51,27 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         searchController.searchBar.placeholder = "Search restaurants..."
         searchController.searchBar.tintColor = UIColor(red: 100.0/255.0, green: 100.0/255.0, blue: 100.0/255.0, alpha: 1.0)
         searchController.searchBar.barTintColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.6)
-
+        
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         navigationController?.hidesBarsOnSwipe = true
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if searchController.active {
             return searchResults.count
@@ -77,7 +79,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             return restaurants.count
         }
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cellIdentifier = "Cell"
@@ -98,8 +100,38 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         return cell
     }
     
+    
+    // Creating a Rotation/Translation Effect Using CATransform3D
+    override func tableView(tableView: UITableView, willDisplayCell cell:
+        UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // Determine if the post is displayed. If yes, we just return and no animation will be created
+        if postShown[indexPath.row] {
+            return
+        }
+        
+        // Indicate the post has been displayed, so the animation won't be displayed again
+        postShown[indexPath.row] = true
+        
+        // Define the initial state (Before the animation)
+        // Rotation
+                let rotationAngleInRadians = 90.0 * CGFloat(M_PI/180.0)
+                let rotationTransform = CATransform3DMakeRotation(rotationAngleInRadians,
+                                                                  0, 0, 1)
+        
+        // Translation
+//        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -500,
+//                                                       100, 0)
+        
+        cell.layer.transform = rotationTransform
+        // Define the final state (After the animation)
+        UIView.animateWithDuration(1.0, animations: { cell.layer.transform =
+            CATransform3DIdentity })
+    }
+    
+    
     // MARK: - Table view delegate
- 
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         
         if editingStyle == .Delete {
@@ -139,10 +171,10 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             }
         })
         
-        // Set the button color
-        shareAction.backgroundColor = UIColor(red: 28.0/255.0, green: 165.0/255.0, blue: 253.0/255.0, alpha: 1.0)
-        deleteAction.backgroundColor = UIColor(red: 202.0/255.0, green: 202.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-
+        // Set the buttons color
+        shareAction.backgroundColor = UIColor(red: 255.0/255.0, green: 166.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+        deleteAction.backgroundColor = UIColor(red: 51.0/255.0, green: 51.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+        
         return [deleteAction, shareAction]
     }
     
@@ -153,10 +185,10 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             return true
         }
     }
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showRestaurantDetail" {
@@ -178,7 +210,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     }
     
     func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-            
+        
         switch type {
         case .Insert:
             if let _newIndexPath = newIndexPath {
@@ -192,18 +224,18 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             if let _indexPath = indexPath {
                 tableView.reloadRowsAtIndexPaths([_indexPath], withRowAnimation: .Fade)
             }
-        
+            
         default:
             tableView.reloadData()
         }
-            
+        
         restaurants = controller.fetchedObjects as! [Restaurant]
     }
     
     func controllerDidChangeContent(controller: NSFetchedResultsController) {
         tableView.endUpdates()
     }
-
+    
     // MARK: - Search
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
@@ -217,7 +249,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         searchResults = restaurants.filter({ (restaurant:Restaurant) -> Bool in
             let nameMatch = restaurant.name.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
             let locationMatch = restaurant.location.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
-        
+            
             return nameMatch != nil || locationMatch != nil
         })
     }
